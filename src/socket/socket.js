cfdba55839e84557df5c0465ca1607499e35293e
Royal_Code <tomburridge2002@gmail.com>
2019-05-13 20:16:01 +0100

    V0.2.9
    Fixed Socket .connect Function

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -8,39 +8,11 @@ class Socket {
         this.setTitleInfo = contextInfo.callbacks.setTitleInfo;
         this.addChatEntry = contextInfo.callbacks.addChatEntry;
 
-        this.state = {
-            socket: io("http://localhost:13371")
-        };
-
-        this.username = localStorage.getItem('username');
-
-        this.state.socket.on('connect', () => {
-            console.log("Connected to Server");
-            this.setTitleInfo("Connected to "+(this.state.socket.io.uri).split('/')[2].split(':')[0]);
-            
-        });
-
-        this.state.socket.on('INFO', (data) => {
-
-            console.log(data);
-            this.setTitleInfo("Requesting for Registering");
-            //console.log("Requesting for Registering");
-            this.state.socket.emit('REGISTER', {username: this.username});
-             
-        });
-
-        this.state.socket.on('MESSAGE_RECEIVED', (data) => {
-            //console.log(data);
-            this.addChatEntry(data);
-        });
-
-        this.state.socket.on('REGISTER', (data) => {
-            var id = data.id;
-            console.log(`${this.username} Registered with ${id}`);
-            console.log("Registering Successful");
-            this.setTitleInfo("Registering Successful");
-        });
-
+        this.socket = io();
+        
+        
+        
+        
 
         // FUNCTION BINDING
         this.connect = this.connect.bind(this);
@@ -49,14 +21,43 @@ class Socket {
     }
 
     sendMessage(msg) {
-        this.state.socket.emit('MESSAGE_SEND', {msg: msg});
+        this.socket.emit('MESSAGE_SEND', {msg: msg});
     }
 
     connect(serverAddress) {
-        this.state.socket.disconnect();
-        this.setTitleInfo("Connecting to "+serverAddress);
-        this.state.socket.connect("http://"+serverAddress+":13371");
+        this.username = localStorage.getItem('username');
+        //console.log(this.username);
+        this.socket.disconnect();
+        this.socket = io('http://'+serverAddress+':13371');
+        this.setTitleInfo("Connecting to "+(this.socket.io.uri).split('/')[2].split(':')[0]);
 
+        this.socket.on('connect', () => {
+            console.log("Connected to Server");
+            this.setTitleInfo("Connected to "+(this.socket.io.uri).split('/')[2].split(':')[0]);
+            
+        });
+
+        this.socket.on('INFO', (data) => {
+
+            console.log(data);
+            this.setTitleInfo("Requesting for Registering");
+            //console.log("Requesting for Registering");
+            this.socket.emit('REGISTER', {username: this.username});
+             
+        });
+
+        this.socket.on('MESSAGE_RECEIVED', (data) => {
+            //console.log(data);
+            this.addChatEntry(data);
+        });
+
+        this.socket.on('REGISTER', (data) => {
+            var id = data.id;
+            console.log(`${this.username} Registered with ${id}`);
+            console.log("Registering Successful");
+            this.setTitleInfo("Registering Successful");
+        });
+        
         //this.state.socket.emit('REGISTER', {username: this.username});
     }
 
